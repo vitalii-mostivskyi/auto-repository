6109ed429dd94a3df91aa4c3c20fce9e04f76ed2
Vitaliy Mostivskiy <lafetaana@gmail.com>
2014-04-28 00:12:26 +0300

    User entity added
    Add user entity? that represents "user" table from database

---
+++ car_shop_administrative_ program/car_shop_administrative_ program/DBConnection/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+
+namespace car_shop_administrative__program.DBConnection.Entities
+{
+    [Table]
+    public class User
+    {
+
+        [Column(IsPrimaryKey = true)]
+        public Guid id { get; set; }
+        
+        [Column]
+        public string name { get; set; }
+
+        [Column]
+        public string surname { get; set; }
+
+        [Column]
+        public string phone { get; set; }
+
+        [Column]
+        public string address { get; set; }
+
+        [Column]
+        public string password { get; set; }
+
+        [Column]
+        public string userType { get; set; }
+    }
+}
