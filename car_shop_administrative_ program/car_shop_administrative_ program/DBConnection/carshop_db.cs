f1716451a7412c51498540001cb4523cd8f0ee7f
VitaliyMostivskiy <lafetaana@gmail.com>
2014-04-28 00:01:28 +0300

    DB connection instance created

---
+++ car_shop_administrative_ program/car_shop_administrative_ program/DBConnection/carshop_db.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+
+namespace car_shop_administrative__program.DBConnection
+{
+    [Database]
+    public class carshop_db: DataContext
+    {
+        carshop_db() : base(Properties.Resources.ConnectionString) { }
+
+       /* public Table<User> Users;
+        public Table<InOutRegister> InOutRegisters;
+        public Table<ActionRegister> ActionRegisters;
+        public Table<Question> Questions;
+        public Table<Answers> AllAnswers;
+        */
+        public static carshop_db Instance = new carshop_db();
+    }
+}
