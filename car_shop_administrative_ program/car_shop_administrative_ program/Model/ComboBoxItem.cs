2727fcc10f7cfa219718222ccf44480015108f48
AndrianTsiselskiy <Tsiselskiy.Andrian@gmail.com>
2014-04-28 19:33:42 +0300

    All sales and cars forms created
    All sales and cars forms created

---
+++ car_shop_administrative_ program/car_shop_administrative_ program/Model/ComboBoxItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace car_shop_administrative__program.Model
+{
+    public class ComboBoxItem
+    {
+        public Guid Value { get; set; }
+
+        public String Text { get; set; }
+    }
+}
